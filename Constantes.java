7a4e4103bd9dc2ba1a51411784ac835a149f4e94
Elo30 <didilou30@hotmail.fr>
2014-03-26 17:55:43 +0100

    Create Constantes.java

---
+++ Constantes.java
@@ -0,0 +1,15 @@
+package moulin;
+
+public class Constantes {
+
+	// constantes
+	public static final int NB_ANIMATIONS_W = 14;
+	public static final int NB_ANIMATIONS_T = 18;
+	public static final int NB_VAISSEAUX = 18;
+	public static final int NB_CASES = 24;
+	public static final int TAILLE_CASE = 70;
+	public static final int ET_EXPL = 2;
+	public static final int ET_DEPL = 1;
+	
+	
+}
