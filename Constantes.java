983859c0db1a624ec3274b08f1813f19dbb9207c
Spiderloco <loic.mercier08@gmail.com>
2014-04-10 17:22:02 +0200

    Update Constantes.java

--- Constantes.java
+++ Constantes.java
@@ -3,6 +3,9 @@ package moulin;
 public class Constantes {
 
 	// constantes
+	public static final int FENETRE_LARGEUR = 1000;
+	public static final int FENETRE_HAUTEUR = 750;
+	public static final int HAUTEUR_PANEL_TOP_BOT = 80;
 	public static final int NB_ANIMATIONS_W = 14;
 	public static final int NB_ANIMATIONS_T = 18;
 	public static final int NB_VAISSEAUX = 18;
