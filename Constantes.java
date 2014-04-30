067bc7e49aa47ea7603748c56e81a97fad8a4887
Spiderloco <loic.mercier08@gmail.com>
2014-04-30 19:29:38 +0200

    Update Constantes.java

--- Constantes.java
+++ Constantes.java
@@ -2,21 +2,24 @@ package moulin;
 
 public class Constantes {
 
-	// constantes
 	public static final int FENETRE_LARGEUR = 1000;
 	public static final int FENETRE_HAUTEUR = 750;
 	public static final int ECART = 80;
 	public static final int HAUTEUR_PANEL_TOP_BOT = 80;
-	public static final int NB_ANIMATIONS_W = 14;
-	public static final int NB_ANIMATIONS_T = 18;
+	public static final int NB_ANIMATIONS_EXPL_W = 14;
+	public static final int NB_ANIMATIONS_EXPL_T = 18;
+	public static final int NB_ANIMATIONS_PLAC_W = 14;
+	public static final int NB_ANIMATIONS_PLAC_T = 18;
 	public static final int NB_VAISSEAUX = 18;
 	public static final int NB_CASES = 24;
 	public static final int TAILLE_CASE = 70;
 	public static final int ET_NULL = 0;
-	public static final int ET_DEPL = 1;
-	public static final int ET_EXPL = 2;
+	public static final int ET_PLAC = 1;
+	public static final int ET_DEPL = 2;
+	public static final int ET_EXPL = 3;
 	public static final int NBR_TIKS_ROT = 6; // 5,6,9 ou 10
 	public static final int NBR_TIKS_DEP = 8; // 5 , 8 ou 10
+	public static final int NBR_TIKS_DEP_LASER = 40;
 	public static final int DIR_HAUT = 0;
 	public static final int DIR_BAS = 1;
 	public static final int DIR_GAUCHE = 2;
@@ -25,12 +28,14 @@ public class Constantes {
 	public static final int PHASE_DE_JEU = 1;
 	public static final int PHASE_DE_DEPLACEMENT = 2;
 	public static final int PHASE_D_EXPLOSION = 3;
-	public static final int PHASE_DE_TIR = 4;
+	public static final int PHASE_DE_CHOIX_CIBLE = 4;
+	public static final int PHASE_DE_TIR = 5;
 	
 	
 	// A metre en variable
 
-	public static final int VITESSE_DEP = 60;
-	public static final int VITESSE_ROT = 30;
+	public static final int VITESSE_ROT = 10;
+	public static final int VITESSE_DEP = 30;
+	public static final int VITESSE_DEP_LASER = 10;
 	
 }
