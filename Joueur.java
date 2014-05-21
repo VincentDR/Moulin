cb1064254f318335967b45e8df11edd94f408d5c
Elo30 <didilou30@hotmail.fr>
2014-05-21 16:18:51 +0200

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -3,35 +3,35 @@ package moulin;
 import java.io.Serializable;
 
 public class Joueur implements Serializable{
-
 	/***************/
 	/***ATTRIBUTS***/
 	/***************/
+	private static final long serialVersionUID = -2788360831910322017L;
 	
+	private int numJoueur;
 
-	
-	
 	/*****************/
 	/***ACCESSEURS****/
 	/*****************/
 	
-	
-	
-	/**
-	 * 
-	 */
+	public int getNumJoueur() {
+		return numJoueur;
+	}
 
+	public void setNumJoueur(int numJoueur) {
+		this.numJoueur = numJoueur;
+	}
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2788360831910322017L;
 
 	/*******************/
 	/***CONSTRUCTEURS***/
 	/*******************/
 	public Joueur() {		
 	
+	}
+	
+	public Joueur(int num){
+		numJoueur = num;
 	}
 
 	public int getNiveau() {
