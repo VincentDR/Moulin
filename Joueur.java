f4282c0237c44e64bcd8ab434715304014f7d455
VincentDR <vincent.de-rousiers@etud.univ-montp2.fr>
2014-05-21 15:37:17 +0200

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -1,6 +1,8 @@
 package moulin;
 
-public class Joueur {
+import java.io.Serializable;
+
+public class Joueur implements Serializable{
 
 	/***************/
 	/***ATTRIBUTS***/
@@ -15,12 +17,28 @@ public class Joueur {
 	
 	
 	
+	/**
+	 * 
+	 */
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2788360831910322017L;
+
 	/*******************/
 	/***CONSTRUCTEURS***/
 	/*******************/
 	public Joueur() {		
 	
-	}	
+	}
+
+	public int getNiveau() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 	
 	/**************/
 	/***METHODES***/
