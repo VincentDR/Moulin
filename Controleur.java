5a142ea1a56033dfb8e4de4b6a4df804683cc9ed
Elo30 <didilou30@hotmail.fr>
2014-05-23 09:53:03 +0200

    Update Controleur.java

--- Controleur.java
+++ Controleur.java
@@ -41,39 +41,43 @@ public class Controleur {
 	
 	
 
-	//Ajoute une piÃ¨ce sur PlaceAJouer du joueur actif
+	// Demande à ajouter une pièce sur PlaceAJouer avec la possession du joueur actif
 	public void AjouterPiece(int placeAJouer)
 	{
 		plateau.AjouterPiece(placeAJouer);
 	}
 
-	// Retourne vrai si au moins 1 déplacement est possible
+	// Retourne vrai si au moins 1 déplacement est possible pour le joueur actif
 	public boolean ExisteDeplacement()
 	{
 		return this.plateau.ExisteDeplacement();
 	}
 	
-	//Retourne 1 si le dÃ©placement est possible, 0 sinon.
+	// Demande à déplacer une pièce de la case placeARetirer vers placeAJouer avec la possession en paramètre
 	public void DeplacerPiece(int placeARetirer, int placeAJouer, int possession)
 	{
 		plateau.DeplacerPiece(placeARetirer, placeAJouer, possession);
 	}
 		
-	public void RetirerPiece(int placeARetirer, int possession)
+	// Demande à retirer la pièce à la case placeARetirer qui doit avoir sa possession = possessionAdv
+	public void RetirerPiece(int placeARetirer, int possessionAdv)
 	{
-		plateau.RetirerPiece(placeARetirer, possession);
+		plateau.RetirerPiece(placeARetirer, possessionAdv);
 	}
 	
+	// Appel de la méthode qui permet de faire jouer le/un ordi
 	public void ordi()
 	{
 		plateau.ControleurOrdi();
 	}
 	
+	// Appel de la méthode qui permet de sauvegarder une partie
 	public void sauvegarder(String nomPartie)
 	{
 		plateau.sauvegarder(nomPartie);
 	}
 	
+	// Appel de la méthode qui permet de charger une partie
 	public void charger(String nomPartie)
 	{
 		try {
@@ -86,63 +90,4 @@ public class Controleur {
 		}
 	}
 	
-
-	
 }
-
-
-
-
-
-
-
-
-/*
-Tableau controle :
-	 * 
-	 * 		
-	 * 		ENVOI PAR LA VUE
-	 * 		Premiere case -> Placement (ou depart deplacement)
-	 * 		Deuxieme case -> Deplacement arrive
-	 * 		Troisieme case -> Piece a eliminer
-	 * 
-	 * 
-	 * 		ENVOI PAR LE MODELE
-	 *  	Premiere case -> Info sur les infos transmises afin d'informer la vue
-	 *  		Result[0] =	-1 ->	Rien
-	 *  				1 ->	Placement
-	 * 				2 ->	Deplacement
-	 * 				3 ->	Moulin
-	 * 				4 ->	Placement+Moulin
-	 * 				5 ->	Deplacement+Moulin
-	 * 
-	 * 		Seconde case -> La possession (1 si action ordi, 2 si action joueur)
-	 * 			Result[1] =	1 ->	Ordi
-	 *  					2 ->	Joueur
-	 *  
-	 *		Troisieme case -> Le placement
-	 * 			Result[2] =	-1 ->	Il ne s'agit pas d'un placement
-	 *  					x ->	La case x ou la piece sera posee
-
-	 * 		Quatrieme case -> Le deplacement, case depart
-	 * 			Result[3] =	-1 ->	Il ne s'agit pas d'un deplacement
-	 *  					x ->	La case x d'ou la piece sera retiree
-	 *  
-	 *  	Quatrieme case -> Le deplacement, case arrivee
-	 * 			Result[4] =	-1 ->	Il ne s'agit pas d'un deplacement
-	 *  					x ->	La case x ou la piece sera posee
-	 *  
-	 *  	Cinquieme case -> Le moulin
-	 * 			Result[5] =	-1 ->	Il ne s'agit pas d'un moulin
-	 *  					x ->	La case x ou la piece sera detruite
-	 *  
-	 *  
-	 *  Resume:
-	 * 			La vue envoie un tableau de 3 entiers, le modele lui repond par un tableau de 6 entiers
-	 * 
-	 * 	Ex: La vue envoie Proposition[0] == Proposition[1] == -1 et Proposition[2] = x
-	 * 		Si x est une piece adverse, alors le modele renverra Result[0] = 3 et Result[5] = x
-	 * 		La vue supprimera donc la case x
-	 * 
-	 */
-
