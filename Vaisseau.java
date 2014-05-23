252c150cbc3e77410d21cf41475b47642183ecba
Spiderloco <loic.mercier08@gmail.com>
2014-05-24 01:34:06 +0200

    Update Vaisseau.java

--- Vaisseau.java
+++ Vaisseau.java
@@ -14,15 +14,18 @@ import javax.swing.JPanel;
 public  class Vaisseau extends JButton  implements Runnable
 {
 	private Panneau monPanneau;
+	private int modePoney;
 	// Les images de l'explosion du vaisseau
     private BufferedImage [] imagesExplosion;
+    private BufferedImage [] imagesExplosionPoney;
 	// Les images du placement du vaisseau
     private BufferedImage [] imagesPlacement;
+    private BufferedImage [] imagesPlacementPoney;
 	// Les images de l'animation moteur du vaisseau
     private BufferedImage [] imagesMoteur;
     
-	private ImageIcon imageOriginale, imageAnimations, imageSelectionne;
-	// Equipe à laquelle le bouton appartient
+	private ImageIcon imageOriginale, imageOriginalePoney, imageAnimations, imageAnimationsPoney, imageSelectionne, imageSelectionnePoney;
+	// Equipe à  laquelle le bouton appartient
 	//private int equipe;
 	 		 // 0 : equipe 1 | 1 : equipe 2
 	private int etat = Constantes.ET_NULL;
@@ -37,7 +40,7 @@ public  class Vaisseau extends JButton  implements Runnable
 	// Ecart entre le vaisseau et la position finale : 1, 2 ou 3
 	private int ecart;
 	private int direction;
-	 		// 0 : Haut | 1 : Bas | Gauche | Droite
+	 		// 0 : Haut |Â 1 : Bas | Gauche | Droite
 	// Déplacement du vaisseau verticalement (en pixel)
 	//private int deplacementY;
 	// Rotation avant déplacement (en degree)
@@ -47,85 +50,152 @@ public  class Vaisseau extends JButton  implements Runnable
 	// permet de savoir si le vaisseau fait partie d'un moulin : 0-Rien 1-Moulin 2-Nouveau moulin
 	private int moulin;
 	//..
-	private int nbrAnimExplosion, nbrAnimPlacement;
-    // Angle du vaisseau par rapport à l'axe vertical
+	private int nbrAnimExplosion, nbrAnimPlacement, nbrAnimPoney;
+    // Angle du vaisseau par rapport à  l'axe vertical
 	private double angle;
 	// INITIALISATION DES FICHIERS IMAGE
     private File [] filesVaisseauExpl;
+    private File [] filesVaisseauExplPoney;
     private File [] filesVaisseauPlac;
+    private File [] filesVaisseauPlacPoney;
 	//private File fileVaisseau = new File("Images/Animations/Explosions/Xplo1.png");
 
 	// Les images du vaisseau
     private BufferedImage imageVaisseau;
+    private BufferedImage imageVaisseauPoney;
     // L'image rotative
     private RotatingImage rotatingImage;
+    private RotatingImage rotatingImagePoney;
 	
 	public Vaisseau(Panneau monPanneau, int equipe, int numero)
 	{
 		 this.monPanneau = monPanneau;
 		 this.equipe = equipe;
 		 this.numero = numero;
+		 this.modePoney = monPanneau.modePoney();
 		 
 		if(equipe==0)
 		{
-			imageOriginale = new ImageIcon("Images/Xwing.png");
-			imageAnimations = new ImageIcon("Images/Xwing.png");
-			imageSelectionne = new ImageIcon("Images/Animations/XwingSelect.png");
-			//filesVaisseau = new File("Images/Animations/Explosions/Xplo1.png");
-			nbrAnimExplosion = Constantes.NB_ANIMATIONS_EXPL_W;
-			nbrAnimPlacement = Constantes.NB_ANIMATIONS_PLAC_W;
+				imageOriginalePoney = new ImageIcon("Images/gummy.png");
+				imageAnimationsPoney = new ImageIcon("Images/gummy.png");
+				imageSelectionnePoney = new ImageIcon("Images/Mlp/gummySelected.png");
+				//filesVaisseau = new File("Images/Animations/Explosions/Xplo1.png");
+				nbrAnimPoney = Constantes.NB_ANIMATIONS_PONEY;
+				
+				imageOriginale = new ImageIcon("Images/Xwing.png");
+				imageAnimations = new ImageIcon("Images/Xwing.png");
+				imageSelectionne = new ImageIcon("Images/Animations/XwingSelect.png");
+				//filesVaisseau = new File("Images/Animations/Explosions/Xplo1.png");
+				nbrAnimExplosion = Constantes.NB_ANIMATIONS_EXPL_W;
+				nbrAnimPlacement = Constantes.NB_ANIMATIONS_PLAC_W;
 		}
 		else
 		{
-			imageOriginale = new ImageIcon("Images/TIE.png");
-			imageAnimations = new ImageIcon("Images/Xwing.png");
-			imageSelectionne = new ImageIcon("Images/Animations/TIEselect.png");
-			//filesVaisseau = new File("Images/Animations/Explosions/Tplo1.png");
-			nbrAnimExplosion = Constantes.NB_ANIMATIONS_EXPL_T;
-			nbrAnimPlacement = Constantes.NB_ANIMATIONS_PLAC_T;
+				imageOriginalePoney = new ImageIcon("Images/angel.png");
+				imageAnimationsPoney = new ImageIcon("Images/angel.png");
+				imageSelectionnePoney = new ImageIcon("Images/Mlp/angelSelected.png");
+				//filesVaisseau = new File("Images/Animations/Explosions/Xplo1.png");
+				nbrAnimPoney = Constantes.NB_ANIMATIONS_PONEY;
+			
+				imageOriginale = new ImageIcon("Images/TIE.png");
+				imageAnimations = new ImageIcon("Images/Xwing.png");
+				imageSelectionne = new ImageIcon("Images/Animations/TIEselect.png");
+				//filesVaisseau = new File("Images/Animations/Explosions/Tplo1.png");
+				nbrAnimExplosion = Constantes.NB_ANIMATIONS_EXPL_T;
+				nbrAnimPlacement = Constantes.NB_ANIMATIONS_PLAC_T;
+			
+			
 		}
 		
 		// Initialisation des fichiers et des images
 		filesVaisseauExpl = new File[nbrAnimExplosion];
 		filesVaisseauPlac = new File[nbrAnimPlacement];
+		filesVaisseauExplPoney = new File[nbrAnimPoney];
+		filesVaisseauPlacPoney = new File[nbrAnimPoney];
 		imagesExplosion = new BufferedImage[nbrAnimExplosion];
 		imagesPlacement = new BufferedImage[nbrAnimPlacement];
+		imagesExplosionPoney = new BufferedImage[nbrAnimPoney];
+		imagesPlacementPoney = new BufferedImage[nbrAnimPoney];
 		
 		// Initialisation de l'animation explosion
 		for(int i=0;i<nbrAnimExplosion;i++)
 		{
-			String chemin="";
-			if(equipe==0)
-			    chemin = "Images/Animations/Explosions/Xplo"+(i+1)+".png";
-			else
-			    chemin = "Images/Animations/Explosions/Tplo"+(i+1)+".png";
-			    //explosionXwing[i-1] = new ImageIcon(rotatingImage);
-			filesVaisseauExpl[i] = new File(chemin);
-			    	
-			try {
+			String chemin ="";
+				if(equipe==0)
+				    chemin = "Images/Animations/Explosions/Xplo"+(i+1)+".png";
+				else
+				    chemin = "Images/Animations/Explosions/Tplo"+(i+1)+".png";
+				    //explosionXwing[i-1] = new ImageIcon(rotatingImage);
+				filesVaisseauExpl[i] = new File(chemin);
+				    	
+				try {
 
-			    imagesExplosion[i] = ImageIO.read(filesVaisseauExpl[i]);
-			} 
-			catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+				    imagesExplosion[i] = ImageIO.read(filesVaisseauExpl[i]);
+				} 
+				catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
 		}
 		
+		for(int i=0;i<nbrAnimPoney;i++)
+		{
+
+			String chemin="";
+				if(equipe==0)
+				    chemin = "Images/Mlp/Gtp"+i+".png";
+				else
+				    chemin = "Images/Mlp/Atp"+i+".png";
+				    //explosionXwing[i-1] = new ImageIcon(rotatingImage);
+				filesVaisseauExplPoney[i] = new File(chemin);
+				    	
+				try {
+
+				    imagesExplosionPoney[i] = ImageIO.read(filesVaisseauExplPoney[i]);
+				} 
+				catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+		}
 		// Initialisation de l'animation placement
 		for(int i=0;i<nbrAnimPlacement;i++)
 		{
+			
+				String chemin ="";
+				if(equipe==0)
+				    chemin = "Images/Animations/Teleportations/Xtp"+i+".png";
+				else
+				    chemin = "Images/Animations/Teleportations/Ttp"+i+".png";
+				
+				filesVaisseauPlac[i] = new File(chemin);
+				    	
+				try {
+
+				    imagesPlacement[i] = ImageIO.read(filesVaisseauPlac[i]);
+				} 
+				catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+		}
+		
+		for(int i=0;i<nbrAnimPoney;i++)
+		{
 			String chemin="";
 			if(equipe==0)
-			    chemin = "Images/Animations/Teleportations/Xtp"+i+".png";
+			    chemin = "Images/Mlp/Gtp"+i+".png";
 			else
-			    chemin = "Images/Animations/Teleportations/Ttp"+i+".png";
+			    chemin = "Images/Mlp/Atp"+i+".png";
 			
-			filesVaisseauPlac[i] = new File(chemin);
+			filesVaisseauPlacPoney[i] = new File(chemin);
 			    	
 			try {
-
-			    imagesPlacement[i] = ImageIO.read(filesVaisseauPlac[i]);
+	
+			    imagesPlacementPoney[i] = ImageIO.read(filesVaisseauPlacPoney[i]);
 			} 
 			catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -142,6 +212,7 @@ public  class Vaisseau extends JButton  implements Runnable
 		try {
 
 			imageVaisseau = ImageIO.read(filesVaisseauExpl[0]);
+			imageVaisseauPoney = ImageIO.read(filesVaisseauExplPoney[0]);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -149,9 +220,11 @@ public  class Vaisseau extends JButton  implements Runnable
 		
 		angle = 0.0;
 		rotatingImage = new RotatingImage(imageVaisseau);
+		rotatingImagePoney = new RotatingImage(imageVaisseauPoney);
 				//this.setIcon(rotatingImage);
 				//setImage(rotatingImage);
 		rotatingImage.setAngleD(angle);
+		rotatingImagePoney.setAngleD(angle);
 				
 	    //imageOriginale.setImage(rotatingImage);
 	    
@@ -184,22 +257,29 @@ public  class Vaisseau extends JButton  implements Runnable
     public  void run()
     {
     	
-    	this.setIcon(imageAnimations);
+    	if(modePoney>0)
+    		this.setIcon(imageAnimationsPoney);
+    	else
+    		this.setIcon(imageAnimations);
+    	
 		System.out.println("Debut fonction run(), x = "+getX());
 		
 
 		if(etat == Constantes.ET_PLAC_1) // etat = placement
 	    {
 			int i = 0 ;
-			System.out.println("nbrAnimPlacement = " + nbrAnimPlacement);
+			if(modePoney>0) nbrAnimPlacement=nbrAnimPoney;
 			while (i < nbrAnimPlacement)
 		       {
 		          try {
 		        	  	
-		        	  
-		        	  	imageAnimations.setImage(imagesPlacement[i]);
+		        	  	if(modePoney>0)
+		        	  		imageAnimationsPoney.setImage(imagesPlacementPoney[i]);
+		        	  	else
+		        	  		imageAnimations.setImage(imagesPlacement[i]);
+		        	  		
 				     	repaint();
-			            Thread.sleep(60) ; // 70
+			            Thread.sleep(Constantes.VITESSE_PLA) ; // 70
 		          	}  
 		          	catch (InterruptedException e) {
 		         
@@ -212,16 +292,20 @@ public  class Vaisseau extends JButton  implements Runnable
 	    }
 		else if(etat == Constantes.ET_PLAC_2) // etat = placement
 	    {
+			if(modePoney>0) nbrAnimPlacement=nbrAnimPoney;
 			int i = nbrAnimPlacement-1 ;
-			System.out.println("nbrAnimPlacement = " + nbrAnimPlacement);
 			while (i >= 0)
 		       {
 		          try {
 		        	  	
 		        	  
-		        	  	imageAnimations.setImage(imagesPlacement[i]);
+		        	  	if(modePoney>0)
+		        	  		imageAnimationsPoney.setImage(imagesPlacementPoney[i]);
+		        	  	else
+		        	  		imageAnimations.setImage(imagesPlacement[i]);
+		        	  	
 				     	repaint();
-			            Thread.sleep(60) ; // 70
+			            Thread.sleep(Constantes.VITESSE_PLA) ; // 70
 		          	}  
 		          	catch (InterruptedException e) {
 		         
@@ -234,7 +318,11 @@ public  class Vaisseau extends JButton  implements Runnable
 	    }
 		else if(etat == Constantes.ET_DEPL) // etat = deplacement
 		{
-	    	this.setIcon(imageOriginale);
+			if(modePoney >0)
+				this.setIcon(imageOriginalePoney);
+			else
+				this.setIcon(imageOriginale);
+			
 			int i =  0 ; 
 			int deplacementX = 0, deplacementY = 0;
 	       
@@ -242,8 +330,17 @@ public  class Vaisseau extends JButton  implements Runnable
 			{
 				try{
 					double oldAngle=0;
-					if(rotatingImage!=null)
-						oldAngle=rotatingImage.getAngleD();
+					if(modePoney>0)
+					{
+						if(rotatingImagePoney!=null)
+							oldAngle=rotatingImagePoney.getAngleD();
+					}
+					else
+					{
+						if(rotatingImage!=null)
+							oldAngle=rotatingImage.getAngleD();
+					}
+						
 					
 					if(direction==Constantes.DIR_GAUCHE)
 					{
@@ -256,7 +353,7 @@ public  class Vaisseau extends JButton  implements Runnable
 					
 					repaint();
 					System.out.println("On tourne");
-					Thread.sleep(Constantes.VITESSE_ROT);//sleep for 20 ms
+					Thread.sleep(Constantes.VITESSE_ROT);
 				}
 				catch(InterruptedException ie){
 					ie.printStackTrace();
@@ -292,7 +389,6 @@ public  class Vaisseau extends JButton  implements Runnable
 	         
 	             // gestion de l'erreur
 	          	}
-				System.out.println("On se déplace");
 				i++;
 	       }
 	       
@@ -302,8 +398,16 @@ public  class Vaisseau extends JButton  implements Runnable
 	       {
 		       try{
 					double oldAngle=0;
-					if(rotatingImage!=null)
-						oldAngle=rotatingImage.getAngleD();
+					if(modePoney>0)
+					{
+						if(rotatingImagePoney!=null)
+							oldAngle=rotatingImagePoney.getAngleD();
+					}
+					else
+					{
+						if(rotatingImage!=null)
+							oldAngle=rotatingImage.getAngleD();
+					}
 					
 					if(direction==Constantes.DIR_GAUCHE)
 					{
@@ -332,6 +436,7 @@ public  class Vaisseau extends JButton  implements Runnable
     	  
     	  int i =  0 ; 
 	       //while (n++ <  100) {
+  		if(modePoney>0) nbrAnimExplosion=nbrAnimPoney;
 	       while (i < nbrAnimExplosion) {
 	          try {
 	        	  //ImageIcon imageTest = new ImageIcon("Images/Animations/blanc.png");
@@ -339,10 +444,14 @@ public  class Vaisseau extends JButton  implements Runnable
 	        	  
 		     	   //this.setIcon(imagesExplosion[i]);
 	        	  //  imageOriginale = imagesExplosion[i]; 
-	        	   imageAnimations.setImage(imagesExplosion[i]);
+	        	  if(modePoney>0)
+	        		  imageAnimationsPoney.setImage(imagesExplosionPoney[i]);
+	        	  else
+	        		  imageAnimations.setImage(imagesExplosion[i]);
+	        		  
 		     	   repaint();
 		     	//repaint(getX(), getY(), getWidth()+20, getHeight()+20);
-	            Thread.sleep(70) ;
+	            Thread.sleep(Constantes.VITESSE_EXPL) ;
 	         }  catch (InterruptedException e) {
 	         
 	             // gestion de l'erreur
@@ -358,7 +467,10 @@ public  class Vaisseau extends JButton  implements Runnable
 
 			System.out.println("Fin fonction run(), x = "+getX());
 			
-	 this.setIcon(imageOriginale);
+	 if(modePoney>0)
+		 this.setIcon(imageOriginalePoney);
+	 else
+		 this.setIcon(imageOriginale);
    }
     
   
@@ -400,11 +512,17 @@ public  class Vaisseau extends JButton  implements Runnable
 		this.selectionne = selectionne;
 		if(selectionne == true)
 		{
-			this.setIcon(imageSelectionne);
+			if(modePoney>0)
+				this.setIcon(imageSelectionnePoney);
+			else
+				this.setIcon(imageSelectionne);
 		}
 		else
 		{
-			this.setIcon(imageOriginale);
+			if(modePoney>0)
+				this.setIcon(imageOriginalePoney);
+			else
+				this.setIcon(imageOriginale);
 		}
 	}
 	
@@ -435,13 +553,27 @@ public  class Vaisseau extends JButton  implements Runnable
 	
 	public void actualiserAngle()
 	{
-		rotatingImage = new RotatingImage(imageVaisseau);
-		//this.setIcon(rotatingImage);
-		//setImage(rotatingImage);
-		rotatingImage.setAngleD(angle);
+		if(modePoney>0)
+		{
+			rotatingImagePoney = new RotatingImage(imageVaisseauPoney);
+			//this.setIcon(rotatingImage);
+			//setImage(rotatingImage);
+			rotatingImagePoney.setAngleD(angle);
+			
+			//////((ImageIcon)this.getIcon()).setImage(rotatingImage);
+			imageOriginalePoney.setImage(rotatingImagePoney);
+		}
+		else
+		{
+			rotatingImage = new RotatingImage(imageVaisseau);
+			//this.setIcon(rotatingImage);
+			//setImage(rotatingImage);
+			rotatingImage.setAngleD(angle);
+			
+			//////((ImageIcon)this.getIcon()).setImage(rotatingImage);
+			imageOriginale.setImage(rotatingImage);
+		}
 		
-		//////((ImageIcon)this.getIcon()).setImage(rotatingImage);
-		imageOriginale.setImage(rotatingImage);
 		//this.setIcon(xWing);
 		repaint();
 	}
@@ -469,14 +601,14 @@ public  class Vaisseau extends JButton  implements Runnable
 	public void calculerRotation(int caseX, int caseY)
 	{
 		//caseDestination
-		// Si le deplacement est à gauche
+		// Si le deplacement est à  gauche
 		if(getX() > caseX)
 		{
 			direction = Constantes.DIR_GAUCHE;
 			ecart = (getX()-caseX)/Constantes.ECART;
 			rotation = 90/Constantes.NBR_TIKS_ROT;
 		}
-			// Si le deplacement est à droite
+			// Si le deplacement est à  droite
 		else if(getX() < caseX)
 		{
 			direction = Constantes.DIR_DROITE;
@@ -499,9 +631,29 @@ public  class Vaisseau extends JButton  implements Runnable
 		}
 	}
 	
+	public void setModePoney(int m)
+	{
+		modePoney = m;
+		if(m>0)
+		{
+			this.setIcon(imageOriginalePoney);
+			imageOriginalePoney.setImage(rotatingImagePoney);
+		}
+		else
+		{
+			imageOriginale.setImage(rotatingImage);
+			this.setIcon(imageOriginale);
+		}
+		
+		repaint();
+	}
+	
 	public void raz()
 	{
-		this.setIcon(imageOriginale);
+		if(modePoney>0)
+			this.setIcon(imageOriginalePoney);
+		else
+			this.setIcon(imageOriginale);
 		this.etat = Constantes.ET_NULL;
 		
 		this.setSelectionne(false);
