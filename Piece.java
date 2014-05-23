0ae57b757c9df581638bb7ffc134e9ce3f46b275
Elo30 <didilou30@hotmail.fr>
2014-05-23 10:21:57 +0200

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -3,44 +3,36 @@ package moulin;
 import java.io.Serializable;
 
 public class Piece implements Serializable {
-	/**
-	 * 
-	 */
+
+
 	private static final long serialVersionUID = 5990060319590476877L;
-	/***************/
-	/***ATTRIBUTS***/
-	/***************/
+
 	private Joueur Proprietaire;
 
 	
-	
-	/*****************/
-	/***ACCESSEURS****/
-	/*****************/
+	// Retourne le Joueur qui possede la piece
 	public Joueur getProprietaire() {
 		return Proprietaire;
 	}
 
-	public void setProprietaire(Joueur proprietaire) {
-		Proprietaire = proprietaire;
+	// Modifie le Joueur propriétaire de la pièce par le Joueur propriet
+	public void setProprietaire(Joueur propriet) {
+		this.Proprietaire = propriet;
 	}
 	
+
 	
-	/*******************/
-	/***CONSTRUCTEURS***/
-	/*******************/
+	// Cré une nouvelle pièce de jeu avec un nouveau joueur propriétaire
 	public Piece(){
-		Proprietaire = new Joueur();
+		this.Proprietaire = new Joueur();
 	}
-	public Piece(Joueur j) {		
-		Proprietaire=j;
-	}
-
-
 	
-	/**************/
-	/***METHODES***/
-	/**************/
+	// Cré une nouvelle pièce de jeu avec un joueur propriétaire égal à j
+	public Piece(Joueur j) {		
+		this.Proprietaire=j;
+	}
+
+	// Retorune le numéro du joueur propriétaire de la pièce de jeu
 	public int getPossession()
 	{
 		return Proprietaire.getNumJoueur();
