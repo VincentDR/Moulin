bc8e651839421909def729ef403ce3e265122ffe
VincentDR <vincent.de-rousiers@etud.univ-montp2.fr>
2014-05-26 18:24:45 +0200

    Update PlateauMoulin.java

--- PlateauMoulin.java
+++ PlateauMoulin.java
@@ -1744,7 +1744,6 @@ public class PlateauMoulin extends Plateau{
 			Result[3] = 9 - (nbPiecesJoueur1+ (9 - (this.getTourDeJeu()+1/2)));
 			Result[4] = 9 - (nbPiecesJoueur2+ (9 - (this.getTourDeJeu()/2)));
 		}else{
-		}else{
 			Result[3] = 9-nbPiecesJoueur1;
 			Result[4] = 9-nbPiecesJoueur2;
 		}
